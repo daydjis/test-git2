57c79318eb9e5fc2d1362344a4ec274e6a535335
Ilya <ilya.mironyuk21gmail.com>
2022-06-21 21:53:39 +0400

    init project

---
+++ src/components/header/MainHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from "./Header.css";
+
+function Header(props) {
+    return (
+        <div>
+            <nav className="header">
+                <div className="header_items">
+
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
