b6d8bd4fb0c1383536df42a7ab5650e21091ae26
Ilya <ilya.mironyuk21gmail.com>
2022-06-21 22:23:49 +0400

    add header and header items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MyButton from "./components/button/MyButton";
 import './styles/App.css';
+import MainHeader from "./components/header/MainHeader";
 
 function App() {
   return (
     <div className="App">
-      <MyButton/>
+        <MainHeader/>
+        <MyButton/>
     </div>
   );
 }
--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import classes from "./Header.css";
 
-function Header(props) {
+function MainHeader(props) {
     return (
         <div>
             <nav className="header">
                 <div className="header_items">
-
+                    <a href="#" className="HeaderItem">Главная</a>
+                    <a href="#" className="HeaderItem">Новости</a>
+                    <a href="#" className="HeaderItem">Задачи</a>
+                    <a href="#" className="HeaderItem">О Проекте</a>
                 </div>
             </nav>
         </div>
     );
 }
 
-export default Header;
+export default MainHeader;
